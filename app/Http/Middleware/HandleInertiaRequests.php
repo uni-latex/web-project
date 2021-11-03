a08aebfd9d431511f0b1d674077bb3248dfd553e
anditsung <anditsung@gmail.com>
2021-11-03 11:07:24 +0700

    fix logo not showing on profile page

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -89,7 +89,7 @@ class HandleInertiaRequests extends Middleware
     {
         $novaLogo = nova_get_setting('logo');
         if ($novaLogo) {
-            return "storage/{$novaLogo}";
+            return "/storage/{$novaLogo}";
         }
         return config('web.logo');
     }
